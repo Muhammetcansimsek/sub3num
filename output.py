6bc16a953d76e11eae174d815cd29672d0202d79
Muhammetcansimsek <simsek-1999@hotmail.com>
2024-01-04 16:30:26 +0300

    update for output

--- output.py
+++ output.py
@@ -3,16 +3,16 @@ class Output:
     def __init__(self) -> None:
          pass
     def status(self, msg1, msg2):
-        print(col.blue + "[*] " + col.end + msg1 + " : " + msg2)
+        print(col.blue + "[*] " + msg1 + " : " + msg2 + col.end)
 
     def good(self, msg1, msg2):
-        print(col.green + "[+] " + col.end + msg1 + " : " + msg2)
+        print(col.green + "[+] " + msg1 + " : " + msg2 + col.end)
 
     def verbose(self, msg1, msg2):
-            print(col.brown + "[v] " + col.end + msg1 + " : " + msg2)
+            print(col.brown + "[v] " + msg1 + " : " + msg2 + col.end)
 
     def warn(self, msg1, msg2):
-        print(col.red + "[-] " + col.end + msg1 + " : " + msg2)
+        print(col.red + "[-] " + msg1 + " : " + msg2 + col.end)
 
     def fatal(self, msg1, msg2):
         print("\n" + col.red + "FATAL: " + msg1 + " : " + msg2 + col.end)
